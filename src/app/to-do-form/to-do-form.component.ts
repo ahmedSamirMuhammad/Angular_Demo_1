f5f58efa80f364869fba4ebc823bd68724ac955b
Ahmed Samir <ahmed.samir00979@gmail.com>
2023-09-03 00:12:34 +0300

    clear commented code

--- src/app/to-do-form/to-do-form.component.ts
+++ src/app/to-do-form/to-do-form.component.ts
@@ -11,25 +11,4 @@ export class ToDoFormComponent {
   addNewTask(value: string) {
     this.newItemEvent.emit(value);
   }
-
-  // addTask() {
-  //   let taskInput = document.getElementById('taskInput') as HTMLInputElement;
-  //   let taskList = document.getElementById('taskList');
-
-  //   if (taskInput.value.trim() !== '' && taskList != null) {
-  //     let listItem = document.createElement('li');
-  //     listItem.className =
-  //       'list-group-item d-flex justify-content-between align-items-center';
-  //     listItem.innerHTML = `
-  //           ${taskInput.value}
-  //           <span>
-  //               <button class="btn btn-success btn-sm mr-2" onclick="completeTask(this)">Complete</button>
-  //               <button class="btn btn-danger btn-sm" onclick="deleteTask(this)">Delete</button>
-  //           </span>
-  //       `;
-
-  //     taskList.appendChild(listItem);
-  //     taskInput.value = '';
-  //   }
-  // }
 }
